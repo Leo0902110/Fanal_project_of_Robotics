b11f10d890ff0100da0a341adee62f0338d49b9c
LFZ121 <liufuzheng121@gmail.com>
2026-05-03 23:38:57 +0800

    Fix optional material env import for Colab render

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -1,3 +1,7 @@
-from src.env.material_pick_cube import MaterialPickCubeEnv, OBJECT_MATERIAL_PROFILES
+try:
+    from src.env.material_pick_cube import MaterialPickCubeEnv, OBJECT_MATERIAL_PROFILES
+except ModuleNotFoundError:
+    MaterialPickCubeEnv = None
+    OBJECT_MATERIAL_PROFILES = {}
 
 __all__ = ["MaterialPickCubeEnv", "OBJECT_MATERIAL_PROFILES"]
